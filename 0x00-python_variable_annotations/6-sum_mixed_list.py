99e9cc5ba63c00798e8e9120157008d51d95039d
Houssame <hossame@inbox.ru>
2024-10-11 18:13:05 +0100

    union

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+sum_mixed
+"""
+
+
+from typing import Union
+
+
+def sum_mixed_list(mxd_lst: Union[int, float]) -> float:
+    """mixed annotation"""
+    return sum(mxd_lst)
