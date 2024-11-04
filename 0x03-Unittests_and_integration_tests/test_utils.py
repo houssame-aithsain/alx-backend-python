d93be6c231b5e641c3914e702b55926d7d74d5c2
Elpira <elpira@elpira.168.1.36>
2024-11-05 00:39:30 +0100

    0

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -15,4 +15,5 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
     def test_access_nested_map(self, nested_map, path, expected):
+        """test access_nested_map function"""
         self.assertEqual(access_nested_map(nested_map, path))
